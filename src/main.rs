b0f53f9d075827b0845b3ec982dd8da9818f6759
973Karljoy <karljoyeux@gmail.com>
2025-01-18 23:09:04 +0100

    Ennemy & star collision system works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{ prelude::*, window::PrimaryWindow};
+use bevy::{ audio::AudioLoader, prelude::*, window::PrimaryWindow};
 use rand::prelude::*;
 pub const PLAYER_SIZE: f32 = 64.;
 pub const PLAYER_SPEED: f32 = 500.;
@@ -19,6 +19,8 @@ pub struct Enemy {
 #[derive(Component)]
 pub struct Star {}
 
+
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -30,16 +32,15 @@ fn main() {
         .add_systems(Update, player_mouvement)
         .add_systems(Update, update_enemy_direction)
         .add_systems(Update, enemy_movement)
+        .add_systems(Update, enemy_hit_player)
+        .add_systems(Update, player_catch_star)
         .run();
 }
 
 fn spawn_camera(mut commands: Commands) {
        commands.spawn((
         Camera2d,
-        Camera {
-            hdr: true, // HDR is required for the bloom effect
-            ..default()
-        }
+        Camera::default()
     ));
 }
 
@@ -56,16 +57,15 @@ fn spawn_enemies(mut commands: Commands, asset_server: Res<AssetServer>, window_
             Err(_) => panic!("No Window found")
         };
         for _ in 0..=ENEMY_COUNT {
-            let random_x = random::<f32>() * window.width()/2.;
-            let random_y = random::<f32>() * window.height()/2.;
+            let random_x: f32 = random::<f32>() * window.width()/2.;
+            let random_y: f32 = random::<f32>() * window.height()/2.;
 
             commands.spawn( 
                 (Sprite::from_image( asset_server.load("sprites/ball_red_large.png")), 
                 Enemy {
                     direction: Vec2::new(random::<f32>(),random::<f32>()).normalize()
                 }, 
-                Transform::from_xyz(random_x, random_y, 0.)));
-            
+                Transform::from_xyz(random_x, random_y, 0.)));    
         } 
 }
 
@@ -90,14 +90,10 @@ fn update_enemy_direction(mut enemy_query:Query<(&Transform, &mut Enemy)>, windo
             let enemy_y_min: f32 =  half_enemy_size - window.height()/2.;
             let enemy_y_max: f32=   window.height()/2. - half_enemy_size ;
 
-            if  translation.x < enemy_x_min ||translation.x > enemy_x_max{ enemy.direction.x *=-1.; direction_changed = true;};
-            if  translation.y<enemy_y_min  ||translation.y > enemy_y_max {enemy.direction.y *= -1.;direction_changed = true;};
+            if  translation.x < enemy_x_min || translation.x > enemy_x_max{ enemy.direction.x *=-1.; direction_changed = true;};
+            if  translation.y<enemy_y_min  || translation.y > enemy_y_max {enemy.direction.y *= -1.;direction_changed = true;};
 
-            if direction_changed {
-                
-            }
-
-
+            if direction_changed {}
         }
 }
 
@@ -163,3 +159,39 @@ pub fn spawn_stars(mut commands: Commands, asset_server: Res<AssetServer>, windo
             
         } 
 }
+
+pub fn enemy_hit_player(mut commands: Commands, mut player_query: Query<(Entity, &Transform), With<Player>>, enemy_query: Query< &Transform, With<Enemy>>,asset_server: Res<AssetServer>) {
+    let impact_distance = (PLAYER_SIZE+ENEMY_SIZE)/2.;
+    //add the audio later 
+    if let Ok(( player_entity, player_transform)) = player_query.get_single_mut() {
+        for enemy_transform in enemy_query.iter() {
+            let distance = player_transform.translation.distance(enemy_transform.translation);
+            if distance <impact_distance {
+                commands.entity(player_entity).despawn();
+                println!("Enemy hit player ! Game Over !")
+            }
+        }
+    }
+
+}
+
+pub fn player_catch_star (mut commands: Commands, mut player_query: Query<(Entity, &Transform), With<Player>>, star_query: Query<(Entity, &Transform), With<Star>>,asset_server: Res<AssetServer>) {
+    let impact_distance = (STAR_SIZE+PLAYER_SIZE)/2.;
+    //add the audio later 
+    if let Ok(( _, player_transform)) = player_query.get_single_mut() {
+        for (star_entity, star_transform) in star_query.iter() {
+            let distance = player_transform.translation.distance(star_transform.translation);
+            if distance <impact_distance {
+                commands.entity(star_entity).despawn();
+                println!("You caught a star !")
+            }
+        }
+    }
+}
+
+// pub fn init_audio_effects (mut commands, asset_server: Res<AssetServer>)
+// {
+//     commands.insert_resource(vec![
+//         asset_server.load::<AudioSource>(path), 
+//     ])
+// }
