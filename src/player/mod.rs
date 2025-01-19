0164b982030b0de77a72e10363ebe7649abb109a
973Karljoy <karljoyeux@gmail.com>
2025-01-19 20:37:05 +0100

    Add ordering in simulation

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -2,7 +2,7 @@ pub mod component;
 pub mod system;
 pub mod resources;
 
-use bevy::app::{App, Plugin, Startup, Update};
+use bevy::{app::{App, Plugin, Startup, Update}, prelude::IntoSystemConfigs};
 pub const PLAYER_SIZE: f32 = 64.;
 pub const PLAYER_SPEED: f32 = 500.;
 
@@ -14,7 +14,7 @@ impl Plugin for PlayerPlugin {
         app
             .add_systems(Startup, spawn_player)
             .add_systems(Update, confine_player_mouvement)
-            .add_systems(Update, player_mouvement)
+            .add_systems(Update, player_mouvement.before(confine_player_mouvement))
             .add_systems(Update, player_catch_star);
     }
 }
